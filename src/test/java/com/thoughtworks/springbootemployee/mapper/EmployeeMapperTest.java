ad7ac4108d5a8e1ab986d14802b62ee79cf207d0
fox <1036674617@qq.com>
2020-08-01 15:58:12 +0800

    [York] feature: add should_return_company_response_when_map_company_response_given_company test case

--- src/test/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapperTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/mapper/EmployeeMapperTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,6 +30,22 @@ public class EmployeeMapperTest {
         assertEquals(employeeRequest.getName(), employee.getName());
         assertEquals(employeeRequest.getGender(), employee.getGender());
         assertEquals(employeeRequest.getSalary(), employee.getSalary());
+    }
+
+    @Test
+    void should_return_company_response_when_map_company_response_given_company() {
+        //given
+        Employee employee = new Employee(1, 18, "female", "York", 1000);
+
+        //when
+        EmployeeResponse employeeResponse = employeeMapper.mapEmployeeResponse(employee);
+
+        //then
+        assertEquals(employee.getId(), employeeResponse.getId());
+        assertEquals(employee.getAge(), employeeResponse.getAge());
+        assertEquals(employee.getGender(), employeeResponse.getGender());
+        assertEquals(employee.getName(), employeeResponse.getName());
+        assertEquals(employee.getSalary(), employeeResponse.getSalary());
     }
 
 }
